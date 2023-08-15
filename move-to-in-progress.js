da1411bb801298dbeab1fcf9b527f7b888cbb412
RhoderickGalero <95960118+RhoderickGalero@users.noreply.github.com>
2023-08-15 16:52:34 +0200

    Update move-to-in-progress.js

--- move-to-in-progress.js
+++ move-to-in-progress.js
@@ -1,22 +1,27 @@
-const { Octokit } = require('@octokit/rest');
+const { getOctokit } = require('@actions/github');
 
 async function main() {
   const token = process.env.PERSONAL_ACCESS_TOKEN;
-  const octokit = new Octokit({ auth: token });
+  const octokit = getOctokit(token);
 
   const issueNumber = process.env.GITHUB_EVENT_PATH
-    ? require('fs').readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8')
-    : '{}';
+    ? JSON.parse(require('fs').readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8')).issue.number
+    : null;
+
+  if (!issueNumber) {
+    console.error('No issue number found.');
+    return;
+  }
 
   try {
     await octokit.issues.update({
       owner: process.env.GITHUB_REPOSITORY.split('/')[0],
       repo: process.env.GITHUB_REPOSITORY.split('/')[1],
-      issue_number: JSON.parse(issueNumber).issue.number,
+      issue_number: issueNumber,
       labels: ['In Progress']
     });
 
-    console.log(`Issue #${JSON.parse(issueNumber).issue.number} moved to In Progress`);
+    console.log(`Issue #${issueNumber} moved to In Progress`);
   } catch (error) {
     console.error('Error:', error.message);
   }
