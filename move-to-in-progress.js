8ce5cf54c2d4764b9a68a53c9881b63efe53e5ae
RhoderickGalero <95960118+RhoderickGalero@users.noreply.github.com>
2023-08-15 16:49:11 +0200

    Update move-to-in-progress.js

--- move-to-in-progress.js
+++ move-to-in-progress.js
@@ -4,17 +4,19 @@ async function main() {
   const token = process.env.PERSONAL_ACCESS_TOKEN;
   const octokit = new Octokit({ auth: token });
 
-  const issueNumber = JSON.parse(process.env.GITHUB_EVENT).issue.number;
+  const issueNumber = process.env.GITHUB_EVENT_PATH
+    ? require('fs').readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8')
+    : '{}';
 
   try {
     await octokit.issues.update({
       owner: process.env.GITHUB_REPOSITORY.split('/')[0],
       repo: process.env.GITHUB_REPOSITORY.split('/')[1],
-      issue_number: issueNumber,
+      issue_number: JSON.parse(issueNumber).issue.number,
       labels: ['In Progress']
     });
 
-    console.log(`Issue #${issueNumber} moved to In Progress`);
+    console.log(`Issue #${JSON.parse(issueNumber).issue.number} moved to In Progress`);
   } catch (error) {
     console.error('Error:', error.message);
   }
