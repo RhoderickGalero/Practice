15be5b6bd0ba3b82d46d117dbeef7a29d874a03f
RhoderickGalero <95960118+RhoderickGalero@users.noreply.github.com>
2023-08-18 21:30:05 +0200

    Update move_to_in_progress.py

--- move_to_in_progress.py
+++ move_to_in_progress.py
@@ -11,7 +11,7 @@ def get_assigned_issues():
     return assigned_issues
 
 def move_to_in_progress(issue_number):
-    command = f"gh issue edit {issue_number} --add-label 'In Progress' --remove-label 'To Do'"
+    command = f"gh issue edit {issue_number} --add-label 'In Progress' --remove-label 'Todo'"
     subprocess.run(command, shell=True)
 
 def main():
@@ -20,7 +20,7 @@ def main():
     for issue in assigned_issues:
         issue_number = issue["number"]
         move_to_in_progress(issue_number)
-        print(f"Moved Issue #{issue_number} to 'In Progress'")
+        print(f"Moved Issue #{issue_number} to 'InProgress'")
 
 if __name__ == "__main__":
     main()
