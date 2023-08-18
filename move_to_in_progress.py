2f88d8e9819fb3985d8d53b6c1c4d4917fe2da6a
RhoderickGalero <95960118+RhoderickGalero@users.noreply.github.com>
2023-08-18 21:14:18 +0200

    Create move_to_in_progress.py

---
+++ move_to_in_progress.py
@@ -0,0 +1,26 @@
+import os
+import subprocess
+import json
+
+def get_assigned_issues():
+    command = "gh issue list --json number,title,state,assignee"
+    result = subprocess.run(command, stdout=subprocess.PIPE, shell=True, text=True)
+    output = result.stdout.strip()
+    issues = json.loads(output)
+    assigned_issues = [issue for issue in issues if issue["assignee"] is not None]
+    return assigned_issues
+
+def move_to_in_progress(issue_number):
+    command = f"gh issue edit {issue_number} --add-label 'In Progress' --remove-label 'To Do'"
+    subprocess.run(command, shell=True)
+
+def main():
+    assigned_issues = get_assigned_issues()
+    
+    for issue in assigned_issues:
+        issue_number = issue["number"]
+        move_to_in_progress(issue_number)
+        print(f"Moved Issue #{issue_number} to 'In Progress'")
+
+if __name__ == "__main__":
+    main()
