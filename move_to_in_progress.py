9e6f5e13c44d69a3cd6b18df03ea76576e610e9f
RhoderickGalero <95960118+RhoderickGalero@users.noreply.github.com>
2023-08-18 23:46:56 +0200

    Update move_to_in_progress.py

--- move_to_in_progress.py
+++ move_to_in_progress.py
@@ -4,15 +4,17 @@ import json
 
 def get_assigned_issues():
 #     command = "gh issue list --json number,title,state,assignees"
-    command = "gh project item-list --format {json} number,title,state,assignees"
+    command = "gh project item-list --owner RhoderickGalero --format json id,assignees"
     result = subprocess.run(command, stdout=subprocess.PIPE, shell=True, text=True)
     output = result.stdout.strip()
     issues = json.loads(output)
     assigned_issues = [issue for issue in issues if issue["assignees"] is not None]
     return assigned_issues
+    print (assigned_issues)
 
 def move_to_in_progress(issue_number):
-    command = f"gh issue edit {issue_number} --add-label 'InProgress' --remove-label 'Todo'"
+ #   command = f"gh issue edit {issue_number} --add-label 'InProgress' --remove-label 'Todo'"
+ #   command = f"gh project item-edit --id issue_id --field-id "PVTSSF_lAHOBbg8Ns4AT02azgMqiD8" --project-id PVT_kwHOBbg8Ns4AT02a --single-select-option-id "47fc9ee4"
     subprocess.run(command, shell=True)
 
 def main():
