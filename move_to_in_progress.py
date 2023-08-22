33cec6b562a8c2d88686bab04bcefdee9cc42362
Rhoderick Galero <rhoderick.galero@ericsson.com>
2023-08-22 08:47:18 +0200

    Update move_to_in_progress.py

--- move_to_in_progress.py
+++ move_to_in_progress.py
@@ -3,17 +3,19 @@ import subprocess
 import json
 
 def get_assigned_issues():
-#     command = "gh issue list --json number,title,state,assignees"
-    command = "gh project item-list --owner RhoderickGalero 6 --format json"
+    command = "gh issue list --json number,title,state,assignees"
+#    command = "gh project item-list --owner RhoderickGalero 6 --format json "
     result = subprocess.run(command, stdout=subprocess.PIPE, shell=True, text=True)
+#   print(result)
     output = result.stdout.strip()
     issues = json.loads(output)
-    assigned_issues = [issue for issue in issues if issue["assignees"] is not None]
+#    print (issues)
+    assigned_issues = [issues for issues in issues if issues["assignees"] is not None]
     return assigned_issues
-    print (assigned_issues)
+#    print (assigned_issues)
 
 def move_to_in_progress(issue_number):
- #   command = f"gh issue edit {issue_number} --add-label 'InProgress' --remove-label 'Todo'"
+    command = f"gh issue edit {issue_number} --add-label 'InProgress' --remove-label 'Todo'"
  #   command = f"gh project item-edit --id issue_id --field-id "PVTSSF_lAHOBbg8Ns4AT02azgMqiD8" --project-id PVT_kwHOBbg8Ns4AT02a --single-select-option-id "47fc9ee4"
     subprocess.run(command, shell=True)
 
