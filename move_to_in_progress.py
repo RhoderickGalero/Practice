4f9762cac152289a08f6f7e5a588fa3594e3bd5b
Rhoderick Galero <rhoderick.galero@ericsson.com>
2023-08-30 13:26:16 +0200

    Update move_to_in_progress.py

--- move_to_in_progress.py
+++ move_to_in_progress.py
@@ -11,20 +11,21 @@ def get_assigned_issues():
     issues = json.loads(output)
 #    print (issues)
     assigned_issues = [issues for issues in issues if issues["assignees"] is not None]
+    print (assigned_issues)
     return assigned_issues
-#    print (assigned_issues)
-
+    
+'''
 def move_to_in_progress(issue_number):
-    command = f"gh issue edit {issue_number} --add-label 'InProgress' --remove-label 'Todo'"
- #   command = f"gh project item-edit --id issue_id --field-id "PVTSSF_lAHOBbg8Ns4AT02azgMqiD8" --project-id PVT_kwHOBbg8Ns4AT02a --single-select-option-id "47fc9ee4"
+   command = f"gh issue edit {issue_number} --add-label 'InProgress' --remove-label 'Todo'"
+   command = f"gh project item-edit --id issue_id --field-id "PVTSSF_lAHOBbg8Ns4AT02azgMqiD8" --project-id PVT_kwHOBbg8Ns4AT02a --single-select-option-id "47fc9ee4"
     subprocess.run(command, shell=True)
-
+'''
 def main():
     assigned_issues = get_assigned_issues()
     
     for issue in assigned_issues:
         issue_number = issue["number"]
-        move_to_in_progress(issue_number)
+#        move_to_in_progress(issue_number)
         print(f"Moved Issue #{issue_number} to 'InProgress'")
 
 if __name__ == "__main__":
