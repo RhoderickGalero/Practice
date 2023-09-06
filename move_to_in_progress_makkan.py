f0834dff8b68aeb1488fa3d9ca70086f88cb311c
Maciej Sokol <maciej.sokol@ericsson.com>
2023-09-06 14:04:57 +0200

    Take 3

--- move_to_in_progress_makkan.py
+++ move_to_in_progress_makkan.py
@@ -3,23 +3,18 @@ import subprocess
 import json
 
 
-def get_assigned_issues():
-    issues = json.loads(run_cmd('gh issue list --json number,title,state,assignees'))
-    assigned_issues = [issues for issues in issues if issues["assignees"] is not None]
-    return assigned_issues
-
-
 def list_project():
     cmd = 'gh project item-list 6 --owner RhoderickGalero'
     return run_cmd(cmd)
 
 
-def move_to_in_progress(issue_nr):
-    cmd = 'gh project item-edit --id issue_id --field-id "PVTSSF_lAHOBbg8Ns4AT02azgMqiD8" --project-id PVT_kwHOBbg8Ns4AT02a --single-select-option-id "47fc9ee4"'
+def move_to_in_progress(issue_id):
+    cmd = 'gh project item-edit --id {0} --field-id "PVTSSF_lAHOBbg8Ns4AT02azgMqiD8" --project-id PVT_kwHOBbg8Ns4AT02a --single-select-option-id "47fc9ee4"'.format(issue_id)
     print(run_cmd(cmd))
     
 
 def run_cmd(cmd):
+    print('Running cmd "{0}"'.format(cmd))
     result = subprocess.run(cmd, stdout=subprocess.PIPE, shell=True, text=True)
     return result.stdout.strip()
 
@@ -30,16 +25,13 @@ def print_envs():
 
 
 def main():
-    print("Envs")
-    print_envs()
-    assigned_issues = get_assigned_issues()
     project_items = list_project()
-    print("Project items")
+    print("Project items:")
     print(project_items)
-    for issue in assigned_issues:
-        issue_number = issue["number"]
-        move_to_in_progress(issue_number)
-        print(f"Moved Issue #{issue_number} to 'InProgress'")
+    issue_nr = os.getenv("ISSUE_NR")
+    #TODO get issue_id from issue_nr
+    move_to_in_progress(issue_nr)
+    print(f"Moved Issue #{issue_nr} to 'InProgress'")
 
 
 if __name__ == "__main__":
