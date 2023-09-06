0ddb42af0c2fefeac4a9089f876acc135d2ef6e2
Maciej Sokol <maciej.sokol@ericsson.com>
2023-09-06 13:50:38 +0200

    Take2

--- move_to_in_progress_makkan.py
+++ move_to_in_progress_makkan.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 import json
 
@@ -6,6 +7,11 @@ def get_assigned_issues():
     issues = json.loads(run_cmd('gh issue list --json number,title,state,assignees'))
     assigned_issues = [issues for issues in issues if issues["assignees"] is not None]
     return assigned_issues
+
+
+def list_project():
+    cmd = 'gh project item-list 6 --owner RhoderickGalero'
+    return run_cmd(cmd)
 
 
 def move_to_in_progress(issue_nr):
@@ -18,13 +24,23 @@ def run_cmd(cmd):
     return result.stdout.strip()
 
 
+def print_envs():
+    for name, value in os.environ.items():
+        print("{0}: {1}".format(name, value))
+
+
 def main():
+    print("Envs")
+    print_envs()
     assigned_issues = get_assigned_issues()
-    
+    project_items = list_project()
+    print("Project items")
+    print(project_items)
     for issue in assigned_issues:
         issue_number = issue["number"]
         move_to_in_progress(issue_number)
         print(f"Moved Issue #{issue_number} to 'InProgress'")
+
 
 if __name__ == "__main__":
     main()
