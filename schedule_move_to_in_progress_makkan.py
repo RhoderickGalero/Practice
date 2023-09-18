38e7b90800f558e8b9cf0cbf6570ec161ae5cd89
RhoderickGalero <95960118+RhoderickGalero@users.noreply.github.com>
2023-09-18 11:28:40 +0200

    Update schedule_move_to_in_progress_makkan.py

--- schedule_move_to_in_progress_makkan.py
+++ schedule_move_to_in_progress_makkan.py
@@ -28,7 +28,7 @@ def main():
     project_items = json.loads(list_items())
     print (project_items)
     for item in project_items["items"]:
-        if item['content']['type'] == "Issue" and item['status'] == "Todo" and item['assignees'] == None:
+        if item['content']['type'] == "Issue" and item['status'] == "Todo" and item['assignees'] is not None:
             #move_to_in_progress(item['id'])
             print(f"Moved Issue (item['id'] to 'InProgress'")
             break
